76198ddc910a5905a100ee648a442cf50ecab60b
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-20 13:00:51 -0800

    modified App.js to include the renderPage logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+      switch (currentPage) {
+          case "About":
+              return <About />;
+          case "Portfolio":
+              return <Portfolio />;
+          case "Contact":
+              return <Contact />;
+          case "Resume":
+              return <Resume />;
+          default:
+              return <About />;
+      }
+  };  
+  
   return (
     <div className="App">
-      <Header></Header>
+      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} ></Header>
+      <main>{renderPage(currentPage)}</main>
       <Footer></Footer>
     </div>
   );

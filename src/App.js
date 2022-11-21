37006479f9215930e21dd48722db8902df4d8173
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-21 14:53:26 -0800

    updated contact and about components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
-import './App.css';
+
 
 function App() {
    
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function Footer() {
   return (
-<div class="card text-center fixed-bottom">
-  <div class="card-body d-flex justify-content-center">
-    <a href="https://github.com/sheetaljwl795" class="btn btn-primary btn-spacing">Github</a>
-    <a href="https://www.linkedin.com/in/sheetaljawale/" class="btn btn-primary btn-spacing">Linkedin</a>    
-  </div>
-</div>
+    <div className="card text-center fixed-bottom">
+      <h6>My Full Stack Development Portfolio - a React site by Sheetal Jawale©</h6>
+      <div className="card-body d-flex justify-content-center">    
+        <a href="https://github.com/sheetaljwl795" className="btn btn-primary btn-spacing">Github</a>
+        <a href="https://www.linkedin.com/in/sheetaljawale/" className="btn btn-primary btn-spacing">Linkedin</a>
+      </div>
+    </div>
   );
-};
+}
+
 
 export default Footer;

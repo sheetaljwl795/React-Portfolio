99d808468cc8dbab9335db5958c20e2394a7ae81
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-21 12:49:58 -0800

    Created About.js file content

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,21 @@
 import React from "react";
+import profileImage from "../assets/SheetalJawale.jpg";
 
 function About() {
   return (
     <div>
-      <p className="content is-medium">Full Stack Developer</p>
-      <hr />
-    
-      <p className="content is-italic mt-4">
-        I'm Sheetal, a software test engineer studying the full stack to move into full stack developer role
-      </p>
+        
+        <h3 style={{ padding: "20px 50px"}}>Full Stack Developer</h3>
+       
+        <div>
+            <img src={profileImage} style={{ width: "25%", float:"left", padding: "50px"}} alt="profile" />
+            
+            <p style={{ width: "50%", float:"left", padding: "50px"}}> 
+            A highly proficient full-stack engineer with experience in web development, unit testing, project management, and problem-solving. Expertise in front-end and back-end technologies, including HTML/CSS, JavaScript/jQuery, Responsive Design, Bootstrap, Node.js, Express.js, React.js, MongoDB, MySQL, API Design, Git, and Progressive Web Applications.
+            </p>
 
-      <p className="content">
-      A highly proficient full-stack engineer with experience in web development, unit testing, project management, and problem-solving. Expertise in front-end and back-end technologies, including HTML/CSS, JavaScript/jQuery, Responsive Design, Bootstrap, Node.js, Express.js, React.js, MongoDB, MySQL, API Design, Git, and Progressive Web Applications.
-      </p>
-      
+        </div>       
+        
     </div>
   );
 }

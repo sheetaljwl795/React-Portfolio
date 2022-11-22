0e9520daa92fc76a7fc6c0e4e3534d2105a95eb8
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-22 10:08:58 -0800

    updated portfolio component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="card text-center fixed-bottom">
+    <div className="text-center fixed-bottom">
       <h6>My Full Stack Development Portfolio - a React site by Sheetal Jawale©</h6>
       <div className="card-body d-flex justify-content-center">    
         <a href="https://github.com/sheetaljwl795" className="btn btn-primary btn-spacing">Github</a>
